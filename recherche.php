0f9579da02c4e40d6693ef9177d51de056a5803a
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-10-15 17:00:23 +0200

    Add search and traitement files

--- recherche.php
+++ recherche.php
@@ -1,2 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>abc-salles</title>
+</head>
+<body>
+    <form enctype="multipart/form-data" action="recherche.php" method="post">
+        <input name="prenom" placeholder="Prénom" type="text">
+        <button>Rechercher</button>
+    </form>
+</body>
+</html>
 <?php
- echo 'hello world';
+$link = mysqli_connect("db-mysql-ams3-63185-do-user-6583675-0.b.db.ondigitalocean.com:25060", "user", "zc0bexxy0extsom1", "abc-salles");
+
+$dump_test =  "SELECT label,type,genre FROM ref_prenoms WHERE label = 'Aaren'";
+foreach  ($link->query($dump_test) as $row) {
+    var_dump('Nom: '. $row['label']);
+    var_dump('Type: '. $row['type']);
+    var_dump('Genre: '. $row['genre']);
+}
+
+$count = 0;
+
+if($_POST['prenom']){
+    $sql =  "SELECT * FROM ref_prenoms WHERE label LIKE '". $_POST['prenom'] . "%' ORDER BY label";
+    echo '<ul>';
+    foreach  ($link->query($sql) as $row) {
+        $type = "";
+        $color = "";
+        $count ++;
+        switch ($row['type']) {
+            case 0:
+                $type = "ambigü";
+                $color = "white";
+                break;
+            case 1:
+                $type = "masculin";
+                $color = "lightskyblue";
+                break;
+            case 2:
+                $type = "feminin";
+                $color = "lightpink";
+                break;
+        }
+        echo "<li style='background: " . $color . "'> ". $row['label'] . " " . $type . " ";
+        switch ($row['genre']) {
+            case 1:
+                echo "masculin ";
+                break;
+            case 2:
+                echo "feminin ";
+                break;
+        }
+        echo nl2br ($row['origin'] . "\n");
+        echo "</li>";
+    }
+    echo "</ul>";
+    echo "Size: ". $count;
+}
---
+++ traitement.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>abc-salles</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="traitement.php" method="post">
+    <input name="file" type="file">
+    <button>Valider</button>
+</form>
+</body>
+</html>
+<?php
+
+
+
+    if ( isset($_FILES["file"])) {
+
+        //if there was an error uploading the file
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+        }
+        else {
+            //Print file details
+            echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+            echo "Type: " . $_FILES["file"]["type"] . "<br />";
+            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+            //if file already exists
+            if (file_exists("upload/" . $_FILES["file"]["name"])) {
+                echo $_FILES["file"]["name"] . " already exists. ";
+            }
+            else {
+                //Store file in directory "upload" with the name of "uploaded_file.txt"
+                $storagename = "uploaded_file.txt";
+                move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $storagename);
+                echo "Stored in: " . "upload/" . $_FILES["file"]["name"] . "<br />";
+            }
+        }
+    } else {
+        echo "No file selected <br />";
+    }
+
+
